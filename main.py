6485233be881abebad359d6f5e9067f961b6be3d
UcanSutlac <ycbandiran@gmail.com>
2020-12-08 00:17:17 +0300

    Train-Test data seperation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# Load CSV
-import csv
-with open('PlantData/Plant_1_Generation_Data.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('PlantData/GenerationData/Plant_1_Generation_Data.csv')
+df['split'] = np.random.randn(df.shape[0], 1)
+
+msk = np.random.rand(len(df)) <= 0.7
+
+train = df[msk]
+test = df[~msk]
+
